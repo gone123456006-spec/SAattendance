504236ea6541afbada2263d9226111b2130d9522
gone123456006-spec <gone123456006@gmail.com>
2025-08-20 17:13:06 +0530

    Delete students.js

--- students.js
+++
@@ -1,7 +0,0 @@
-// students.js
-const students = {
-    "123": { name: "John Doe", class: "10A", phone: "9876543210" },
-    "124": { name: "Jane Smith", class: "10B", phone: "9876543211" },
-    "125": { name: "Ravi Kumar", class: "9A", phone: "9876543212" }
-};
-    
